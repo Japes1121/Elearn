60816bdca600ff3a7da197ff016b66e6c6d2b08f
jayaprakash.j <jayaprakash.j@zohocorp.com>
2022-03-14 23:11:14 +0530

    jersey dependency added

---
+++ src/main/java/com/elearn/resource/TestResource.java
@@ -0,0 +1,15 @@
+package com.elearn.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+
+@Path("/test")
+public class TestResource {
+
+	@GET
+	@Produces("text/plain")
+	public String getClichedMessage() {
+		return "This is my First Rest API Call!";
+	}
+}
