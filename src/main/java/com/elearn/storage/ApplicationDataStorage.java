1901b81232ba5698dffbf544a6a4783b114d2fba
priyasivasubramanian <90206883+priyasivasubramanian@users.noreply.github.com>
2022-03-14 15:05:23 +0530

    changes

--- src/main/java/com/elearn/storage/ApplicationDataStorage.java
+++ src/main/java/com/elearn/storage/ApplicationDataStorage.java
@@ -13,16 +13,61 @@ public class ApplicationDataStorage {
 		MySQLConnector.closeConnection(con);
 	}
 
-	static void addCategory(String categoryName) {
+	static void addCategory(String CATEGORY_NAME) {
 		Connection con = getConnection();
 		try {
 			Statement stmt = con.createStatement();
+			String query = "insert into CATEGORY (CATEGORY_NAME)" + "VALUES (" + CATEGORY_NAME + ")";
+			
+		 stmt.execute(query);
+		
 		} catch (Exception e) {
-
 		} finally {
 			closeConnection(con);
 		}
-
 	}
 
+	static void addDepartment(int CATEGORY_ID, int DEPARTMENT_ID, String DEPARTMENT_NAME) {
+		Connection con = getConnection();
+		try {
+			Statement stmt = con.createStatement();
+			String query = "insert into DEPARTMENT (CATEGORY_ID,DEPARTMENT_ID,DEPARTMENT_NAME)" + "VALUES ('1',11,'CSE&IT' )";
+			stmt.executeUpdate(query);
+			query = "insert into DEPARTMENT (CATEGORY_ID,DEPARTMENT_ID,DEPARTMENT_NAME)" + "VALUES ('2', 12,'ECE')";
+			stmt.executeUpdate(query);
+			query = "insert into DEPARTMENT (CATEGORY_ID,DEPARTMENT_ID,DEPARTMENT_NAME)" + "VALUES ('3',13,'EEE')";
+			stmt.executeUpdate(query);
+			query = "insert into DEPARTMENT (CATEGORY_ID,DEPRTMENT_ID,DEPARTMENT_NAME)" + "VALUES ('4',14,'MECH')";
+			stmt.executeUpdate(query);
+			query = "insert into DEPARTMENT (CATEGORY_ID,DEPARTMENT_ID,DEPARTMENT_NAME)" + "VALUES ('5',15,'CIVIL')";
+			stmt.executeUpdate(query);
+			
+			
+			stmt.execute(query);
+			
+			
+			
+		} catch (Exception e) {
+ 
+		} finally {
+			closeConnection(con);
+		}
+	}
+	
+	static void addSubject( int DEPARTMENT_ID, int SUBJECT_ID, String SUBJECT_CODE,String SUBJECT_NAME) {
+		Connection con = getConnection();
+		try {
+			Statement stmt = con.createStatement();
+			String query = "insert into SUBJECT (DEPARTMENT_ID,SUBJECT_ID,SUBJECT_CODE,SUBJECT_NAME)" + "VALUES ('11',101," + SUBJECT_CODE +","+SUBJECT_NAME+")";
+			stmt.executeUpdate(query);
+			
+			
+			stmt.execute(query);
+		} catch (Exception e) {
+			 
+		} finally {
+			closeConnection(con);
+		}
+	}
 }
+
