7a587d89fbdd1de2b8e3d68f8065c1b8fb54d0dc
priyasivasubramanian <90206883+priyasivasubramanian@users.noreply.github.com>
2022-03-14 18:17:42 +0530

    changes

--- src/main/java/com/elearn/storage/ApplicationDataStorage.java
+++ src/main/java/com/elearn/storage/ApplicationDataStorage.java
@@ -13,61 +13,147 @@ public class ApplicationDataStorage {
 		MySQLConnector.closeConnection(con);
 	}
 
-	static void addCategory(String CATEGORY_NAME) {
+	static void addCategory(String categoryName) {
 		Connection con = getConnection();
 		try {
 			Statement stmt = con.createStatement();
-			String query = "insert into CATEGORY (CATEGORY_NAME)" + "VALUES (" + CATEGORY_NAME + ")";
-			
-		 stmt.execute(query);
+			String query = "insert into CATEGORY (CATEGORY_NAME)" + "VALUES (" + categoryName + ")";
 		
-		} catch (Exception e) {
-		} finally {
-			closeConnection(con);
-		}
-	}
-
-	static void addDepartment(int CATEGORY_ID, int DEPARTMENT_ID, String DEPARTMENT_NAME) {
-		Connection con = getConnection();
-		try {
-			Statement stmt = con.createStatement();
-			String query = "insert into DEPARTMENT (CATEGORY_ID,DEPARTMENT_ID,DEPARTMENT_NAME)" + "VALUES ('1',11,'CSE&IT' )";
-			stmt.executeUpdate(query);
-			query = "insert into DEPARTMENT (CATEGORY_ID,DEPARTMENT_ID,DEPARTMENT_NAME)" + "VALUES ('2', 12,'ECE')";
-			stmt.executeUpdate(query);
-			query = "insert into DEPARTMENT (CATEGORY_ID,DEPARTMENT_ID,DEPARTMENT_NAME)" + "VALUES ('3',13,'EEE')";
-			stmt.executeUpdate(query);
-			query = "insert into DEPARTMENT (CATEGORY_ID,DEPRTMENT_ID,DEPARTMENT_NAME)" + "VALUES ('4',14,'MECH')";
-			stmt.executeUpdate(query);
-			query = "insert into DEPARTMENT (CATEGORY_ID,DEPARTMENT_ID,DEPARTMENT_NAME)" + "VALUES ('5',15,'CIVIL')";
-			stmt.executeUpdate(query);
-			
-			
 			stmt.execute(query);
 			
-			
-			
 		} catch (Exception e) {
- 
+			e.printStackTrace();
 		} finally {
 			closeConnection(con);
 		}
 	}
 	
-	static void addSubject( int DEPARTMENT_ID, int SUBJECT_ID, String SUBJECT_CODE,String SUBJECT_NAME) {
+	static void UpdateCategory(String categoryName) {
 		Connection con = getConnection();
 		try {
 			Statement stmt = con.createStatement();
-			String query = "insert into SUBJECT (DEPARTMENT_ID,SUBJECT_ID,SUBJECT_CODE,SUBJECT_NAME)" + "VALUES ('11',101," + SUBJECT_CODE +","+SUBJECT_NAME+")";
-			stmt.executeUpdate(query);
-			
-			
+			String query = "(Update category set catgoryName where categoryId =1)";
+		
 			stmt.execute(query);
+			
 		} catch (Exception e) {
-			 
+			e.printStackTrace();
 		} finally {
 			closeConnection(con);
 		}
 	}
+	
+	
+	static void deleteCategory(String categoryName) {
+		Connection con = getConnection();
+		try {
+			Statement stmt = con.createStatement();
+			String query = "(delete from category  where categoryId =1)";
+		
+			stmt.execute(query);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeConnection(con);
+		}
+	}
+	
+
+
+	
+	
+	static void addDepartment(int categoryId, String departmentName) {
+		Connection con = getConnection();
+		try {
+			Statement stmt = con.createStatement();
+			String query = "insert into DEPARTMENT (CATEGORY_ID,DEPARTMENT_NAME)" + "VALUES (" + categoryId + ","
+					+ departmentName + ")";
+			stmt.execute(query);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeConnection(con);
+		}
+	}
+
+	
+	static void updateDepartment(int categoryId, String departmentName) {
+		Connection con = getConnection();
+		try {
+			Statement stmt = con.createStatement();
+			String query = "(Update department set departmentName where departmentId =1)";
+			stmt.execute(query);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeConnection(con);
+		}
+	}
+
+	
+	static void deleteDepartment(int categoryId, String departmentName) {
+		Connection con = getConnection();
+		try {
+			Statement stmt = con.createStatement();
+			String query = "(delete from department  where departmentId =1)";
+			stmt.execute(query);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeConnection(con);
+		}
+	}
+
+	
+	
+	
+	
+	static void addSubject(int departmentId, String subjectCode, String subjectName) {
+		Connection con = getConnection();
+		try {
+			Statement stmt = con.createStatement();
+			String query = "insert into SUBJECT (DEPARTMENT_ID,SUBJECT_CODE,SUBJECT_NAME)" + "VALUES (" + departmentId
+					+ "," + subjectCode + "," + subjectName + ")";
+			stmt.execute(query);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeConnection(con);
+		}
+	}
+
+
+	static void updateSubject(int departmentId, String subjectCode, String subjectName) {
+	Connection con = getConnection();
+	try {
+		Statement stmt = con.createStatement();
+		String query = "(Update Subject setsubjectName where subjectId =1)";
+		query = "(Update Subject set subjectCode where subjectId =1)";
+		stmt.execute(query);
+	} catch (Exception e) {
+		e.printStackTrace();
+	} finally {
+		closeConnection(con);
+	}
+ }
+
+
+
+
+	static void deleteSubject(int departmentId, String subjectCode, String subjectName) {
+	Connection con = getConnection();
+	try {
+		Statement stmt = con.createStatement();
+		String query = "(delete from subject where subjectId =1)";
+		stmt.execute(query);
+	} catch (Exception e) {
+		e.printStackTrace();
+	} finally {
+		closeConnection(con);
+	}
+ }
+
 }
+
 
