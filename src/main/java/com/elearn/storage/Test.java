33c00cd44766a684590d472d240d454b99aa1414
jayaprakash.j <jayaprakash.j@zohocorp.com>
2022-03-15 10:44:37 +0530

    test method added

--- src/main/java/com/elearn/storage/Test.java
+++ src/main/java/com/elearn/storage/Test.java
@@ -1,9 +1,25 @@
 package com.elearn.storage;
 
+import java.util.List;
+
+import com.elearn.storage.model.Category;
+
 public class Test {
 
 	public static void main(String[] args) {
+		testAddCategory("Dummy");
+		getAllCategories();
+	}
 
+	private static void testAddCategory(String categoryName) {
+		ApplicationDataStorage.addCategory(categoryName);
+	}
+
+	private static void getAllCategories() {
+		List<Category> categoryList = ApplicationDataStorage.getAllCategories();
+		for (Category category : categoryList) {
+			System.out.println(category.getCategoryId() + " == " + category.getCategoryName());
+		}
 	}
 
 }
