d5e982ac19905a2f9dd6438748dedffb092474ea
priyasivasubramanian <90206883+priyasivasubramanian@users.noreply.github.com>
2022-03-15 22:51:53 +0530

    Notes.java

---
+++ src/main/java/com/elearn/storage/model/Notes.java
@@ -0,0 +1,78 @@
+package com.elearn.storage.model;
+
+import java.sql.Blob;
+
+public class Notes {
+	private int subjectId;
+	private int notesId;
+	private Blob content;
+	private int postedBy;
+	private String updatedBy;
+	private String uploadedBy;
+	private String updatedTime;
+	private String uploadedTime;
+
+	public int getSubjectId() {
+		return subjectId;
+	}
+
+	public void setSubjectId(int subjectId) {
+		this.subjectId = subjectId;
+	}
+
+	public int getNotesId() {
+		return notesId;
+	}
+
+	public void setNotesId(int notesId) {
+		this.notesId = notesId;
+	}
+
+	public Blob getContent() {
+		return content;
+	}
+
+	public void setContent(Blob content) {
+		this.content = content;
+	}
+
+	public int getPostedBy() {
+		return postedBy;
+	}
+
+	public void setPostedBy(int postedBy) {
+		this.postedBy = postedBy;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public String getUploadedBy() {
+		return uploadedBy;
+	}
+
+	public void setUploadedBy(String uploadedBy) {
+		this.uploadedBy = uploadedBy;
+	}
+
+	public String getUpdatedTime() {
+		return updatedTime;
+	}
+
+	public void setUpdatedTime(String updatedTime) {
+		this.updatedTime = updatedTime;
+	}
+
+	public String getUploadedTime() {
+		return uploadedTime;
+	}
+
+	public void setUploadedTime(String uploadedTime) {
+		this.uploadedTime = uploadedTime;
+	}
+}
