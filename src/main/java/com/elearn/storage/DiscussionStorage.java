0f22ce25c003b55dd6e8c5a089bfcb6e65d2bea9
MAHALAKSHMI SRIDHAR <mahalakshmisridhar@MAHALAKSHMIs-MacBook-Air.local>
2022-03-15 21:33:32 +0530

    changes

--- src/main/java/com/elearn/storage/DiscussionStorage.java
+++ src/main/java/com/elearn/storage/DiscussionStorage.java
@@ -1,5 +1,65 @@
 package com.elearn.storage;
 
-public class DiscussionStorage {
+import java.sql.Connection;
+import java.sql.Statement;
 
+public class DiscussionStorage {
+	private static Connection getConnection() {
+		return MySQLConnector.getMySQLConnection();
+	}
+	private static void closeConnection(Connection con) {
+		MySQLConnector.closeConnection(con);
+	}
+	
+	static void addDiscussion(int notesId, String postedBy) {
+		Connection con = getConnection();
+		try {
+			Statement stmt = con.createStatement();
+			String query = "INSERT INTO DISCUSSION (NOTES_ID,POSTED_BY)" + "VALUES(" + notesId + ", " + postedBy + ")";		
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeConnection(con);
+		}
+	}
+
+
+	static void updateDiscussion(int discussionId, String postedBy, String createdTime, String modifiedTime) {
+		Connection con = getConnection();
+		try {
+			Statement stmt = con.createStatement();
+			StringBuilder bldr = new StringBuilder("UPDATE DISCUSSION SET");
+			if(postedBy != null) {
+				bldr.append("POSTED_BY = '").append(postedBy).append("' ");
+			}
+			if(createdTime != null) {
+			bldr.append("CREATED_TIME = '").append(createdTime).append("' ");
+			}
+			if(modifiedTime != null) {
+				bldr.append("MODIFIED_TIME = '").append(modifiedTime).append("' ");
+			}
+			bldr.append(" WHERE DISCUSSION_ID = ").append(discussionId);
+			stmt.executeUpdate(bldr.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeConnection(con);
+		}
+	}
+			
+			
+			
+	
+	static void deleteDisussion(int discussionId) {
+	Connection con = getConnection();
+	try {
+		Statement stmt = con.createStatement();
+		String query = "DELETE FROM DISCUSSION WHERE DISCUSSION_ID = " + discussionId;
+		stmt.executeUpdate(query);
+	} catch (Exception e) {
+		e.printStackTrace();
+	} finally {
+		closeConnection(con);
+	}
 }
+	}
